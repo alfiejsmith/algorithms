dd64c0705b1f8de05930a58baa68a6e46d18f575
Alfie Smith <alfie@alfiesmith.net>
2020-10-18 13:58:28 +0100

    Bogo sort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from search.binary.binary_search import run as binary_search
 from search.linear.linear_search import run as linear_search
 
+from sort.bogo.bogo_sort import run as bogo_sort
 from sort.bubble.bubble_sort import run as bubble_sort
 from sort.insertion.insertion_sort import run as insertion_sort
 
@@ -8,6 +9,7 @@ from sort.insertion.insertion_sort import run as insertion_sort
 algorithms = {
     "Binary Search": binary_search,
     "Linear Search": linear_search,
+    "Bogo Sort": bogo_sort,
     "Bubble Sort": bubble_sort,
     "Insertion Sort": insertion_sort
 }
